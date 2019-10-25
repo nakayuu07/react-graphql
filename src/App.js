14305866bef5aaa90707ae45cb465c59cf493609
yuya nakamura <nakayuu07@gmail.com>
2019-10-26 02:06:44 +0900

    change: query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import client from './client'
 import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
-import { ME } from './graphql'
+import { SEARCH_REPOSITORIES } from './graphql'
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <p>Hello React GraphQl</p>
-      <Query query={ME}>
-        {
-          ({loading, error, data}) => {
-            if (loading) return '...loading'
-            if (error) return `Error ${error.message}`
-            
-            return <img src = {data.user.avatarUrl}></img>
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア"
+}
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = VARIABLES
+  }
+
+  render(){
+    const { first, after, last, before, query } = this.state
+    return (
+      <ApolloProvider client={client}>
+        <Query 
+          query={SEARCH_REPOSITORIES}
+          variables={{ first, after, last, before, query }}
+        >
+          {
+            ({loading, error, data}) => {
+              if (loading) return '...loading'
+              if (error) return `Error ${error.message}`
+              
+              console.log(data)
+              return<div></div>
+            }
           }
-        }
-      </Query>
-    </ApolloProvider>
-  );
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
