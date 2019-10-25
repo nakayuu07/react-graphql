68ad28bd4247749b5d20e2bd524ede9bcd985c10
yuya nakamura <nakayuu07@gmail.com>
2019-10-26 02:06:10 +0900

    add: query

--- src/graphql.js
+++ src/graphql.js
@@ -7,3 +7,30 @@ export const ME = gql`
     }
   }
 `
+
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($first: Int, $last: Int, $after: String, $before: String ,$query: String!){
+    search(first: $first, last: $last, after: $after, before: $before , query: $query, type: REPOSITORY){
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges{
+        cursor
+        node{
+          ...on Repository {
+            id
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`
